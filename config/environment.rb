3e7806ffa5240bd43d2fb4ecae19a4d4f967e27e
Ben Aneesh <benaneesh@gmail.com>
2012-08-08 10:18:41 +0400

    init

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
 # Initialize the rails application
 Corp::Application.initialize!
